a4973867ba6d8a165eab3d74f0633d7ec63dcf97
dawsonmyers <dawsonmyers@geotab.com>
2021-03-03 09:56:04 -0500

    Uncomment line

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 
 
 if [[ -n `grep dev-env-config ~/.bashrc` ]]; then
