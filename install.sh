753224d2e09b0939d4bbe90d36beb6bc6f04cba9
dawsonmyers <dawsonmyers@geotab.com>
2021-06-09 10:46:08 -0400

    Add default prompt, update install script

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 
 
-if [[ -n `grep dev-env-config ~/.bashrc` ]]; then
+# Remove content starting at "#geo-cli-start" and ending
+# at "#geo-cli-end" comments.
+sed -i '/#dev-env-config-start/,/#dev-env-config-end/d' ~/.bashrc
+
+# if [[ -n `grep dev-env-config ~/.bashrc` ]]; then
 
 #	tee -a $HOME/.bashrc <<- EOF
 	cat <<- EOF >> $HOME/.bashrc
 	
-	# Dev environment config repo bootsrap
+	#dev-env-config-start
+	# Dev environment config repo bootstrap
 	#######################################
 	. ~/dev-env-config/bashrc.sh
 	#######################################
+	#dev-env-config-end
 	EOF
-fi
+# fi
